ce04f1e436043f74e184ae19d9912a19d84283e4
Achraf <achrafatauil@hotmail.com>
2020-10-23 13:54:13 +0200

    Move code inside app.patch into one main function (see description)
    - cleanup structure
    - create a main function to keep entry file (app.js) clean

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 "use strict";
 import { app, errorHandler } from 'mu';
-const { default: Axios } = require("axios");
-const { default: getEmails } = require('./services/getEmails')
-const { default: sendEmails } = require('./services/sendEmails')
+const { default: Axios } = require('axios');
+const { default: main } = require('./services/main');
 
 var CronJob = require('cron').CronJob;
 
-const cronFrequency =  '*/10 * * * * *';
+const cronFrequency =  '*/20 * * * * *';
 
 new CronJob(cronFrequency, function() {
   console.log(`*************************************************************************`);
@@ -16,27 +15,7 @@ new CronJob(cronFrequency, function() {
 }, null, true);
 
 app.patch('/email-delivery/', async function(req, res, next) {
-  console.log(" >>> Step 1: Find & Retrieve Emails from the database.")
-  let getClass = new getEmails()
-  let emails = await getClass.retrieveEmails();
-
-  if (emails.length == 0){
-    console.log("*** No Emails found to be send. Stopping after step 1. ***")
-    return res.status(204).end()
-  }
-
-  console.log(` >  ${emails.length} emails found that need to be send. `)
-  console.log(` >>> Step 2: Start sending emails using ${process.env.SMTP_OR_REST}`)
-
-  let sendClass = new sendEmails()
-  let count = 0;
-  emails.forEach(async email => {
-      count++;
-      console.log(` > Sending email ${count} out of ${emails.length} `)      
-      await sendClass.sendEmail(email)
-  });
-
-  console.log("Done")
+    await main()
 });
 
 
