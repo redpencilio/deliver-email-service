da13bb1a5436bbbcef04936440d1bc047e10ea45
Achraf <achrafatauil@hotmail.com>
2020-10-26 16:49:46 +0100

    Remove bugs + add move email to sending folder (ssee description)
    - move email to sending folder while it is being send 
    - add count to failed or success loggin for better tracking multiple emails
    - add try catch when sending

--- services/protocols/SMTP.js
+++ services/protocols/SMTP.js
@@ -2,23 +2,27 @@
 const { default: nodemailerServices } = require('../../data/nodeMailerServices');
 const { default: moveEmailToFolder } = require("../../queries/moveEmailToFolder");
 const { default: updateEmailId } = require('../../queries/updateEmailId');
+const { default: createSentDate } = require('../../queries/createSentDate');
 const nodemailer = require("nodemailer");
 
 // ENV
 const wkServiceOrServer = process.env.WELL_KNOWN_SERVICE_OR_SERVER;
 const fromName = process.env.FROM_NAME || '';
 const hoursDeliveringTimeout = process.env.HOURS_DELIVERING_TIMEOUT || 1;
+const graph = process.env.GRAPH_NAME || 'http://mu.semte.ch/graphs/system/email';
 
 // MAIN FUNCTION
 function smtp(emails) {
   let count = 0;
   emails.forEach(async email => {
     count++
-    console.log(` > Sending Email ${count} out of ${emails.length}`)
     try {
-      checkSendDate(email)
-      checkTimeout(email)
-      await sendMail(email)
+      _sendEmailToSending(email)
+      _checkSentDate(email)
+      
+      _checkTimeout(email)
+      
+      _sendMail(email, count)
     } catch (err) {
       console.log(err)
     }
@@ -26,25 +30,32 @@ function smtp(emails) {
 }
 
 // SUB FUNCTIONS CALLED BY MAIN
-async function checkSendDate(email) {
-  if (!email.sentDate) {
-    await createSentDate(this.graph, email)
+
+async function _sendEmailToSending(email){
+  await moveEmailToFolder(graph, email.uuid, "sending");
+}
+
+async function _checkSentDate(email) {
+  if (!email.sentDate || email.sentDate == "") {
+    await createSentDate(graph, email)
+
     console.log(` > No send date found, a send date has been created.`)
   }
 }
 
-async function checkTimeout(email) {
+async function _checkTimeout(email) {
   let modifiedDate = new Date(email.sentDate);
   let currentDate = new Date();
   let timeout = ((currentDate - modifiedDate) / (1000 * 60 * 60)) <= parseInt(hoursDeliveringTimeout);
 
   if (timeout) {
     await moveEmailToFolder(graph, email.uuid, "failbox");
+    ;
     throw `*** FAILED: Timeout reached, message moved to failbox: ${email.uuid} ***`;
   }
 }
 
-async function sendMail(email) {
+async function _sendMail(email, count) {
   let transporter = null;
 
   if (!((nodemailerServices.indexOf(wkServiceOrServer) > (-1)) || (nodemailerServices == 'server'))) {
@@ -93,22 +104,31 @@ async function sendMail(email) {
     attachments: attachments
   };
 
-  transporter.sendMail(mailProperties, async (failed, success) => {
-    if (failed) {
+  try{
 
-      // await moveEmailToFolder(graph, email.uuid, "failbox");
-      console.log(" > FAILED: The destination server responded with an error. Email send to Failbox.")
-      console.log(` > ${failed}`)
-    }
+    transporter.sendMail(mailProperties, async (failed, success) => {
+      if (failed) {
 
-    if (!failed) {
-      console.log(` > Email UUID:`, email.uuid);
-      await moveEmailToFolder(graph, email.uuid, "sentbox");
-      console.log(` > Message moved to sentbox: ${email.uuid}`);
-      await updateEmailId(graph, email.messageId, success.messageId);
-      console.log(` > MessageId updated from ${email.messageId} to ${success.messageId}`);
-      email.messageId = success.messageId;
-    }
-  });
+        await moveEmailToFolder(graph, email.uuid, "failbox");
+        
+        console.log(` > The destination server responded with an error. Email ${count} send to Failbox.`)
+        console.log(` > ${failed}`)
+
+      } else {
+        console.log(` > Email ${count} UUID:`, email.uuid);
+        await moveEmailToFolder(graph, email.uuid, "sentbox");
+        console.log(` > Message moved to sentbox: ${email.uuid}`);
+        await updateEmailId(graph, email.messageId, success.messageId);
+        console.log(` > MessageId updated from ${email.messageId} to ${success.messageId}`);
+        email.messageId = success.messageId;
+      }
+    })
+  }
+
+  catch(err) {
+    console.log(err)
+  }
+    
 }
+
 export default smtp;
