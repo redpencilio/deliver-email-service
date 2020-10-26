3241b5eba5a1e6740d6f1fbd6508a81bdb13a0fe
Achraf <achrafatauil@hotmail.com>
2020-10-26 16:50:12 +0100

    Create test file option that will create temp email account for testing

--- services/protocols/TEST.js
+++ services/protocols/TEST.js
@@ -1,5 +1,111 @@
-function test(email){
-  // TODO: Add testing environment
+
+// IMPORTS
+const { default: nodemailerServices } = require('../../data/nodeMailerServices');
+const { default: moveEmailToFolder } = require("../../queries/moveEmailToFolder");
+const { default: updateEmailId } = require('../../queries/updateEmailId');
+const { default: createSentDate } = require('../../queries/createSentDate');
+const nodemailer = require("nodemailer");
+
+// ENV
+const fromName = process.env.FROM_NAME || '';
+const hoursDeliveringTimeout = process.env.HOURS_DELIVERING_TIMEOUT || 1;
+const graph = process.env.GRAPH_NAME || 'http://mu.semte.ch/graphs/system/email';
+
+// MAIN FUNCTION
+function test(emails){
+  console.log(" >>> Sending mails in Testing Environment")
+  let count = 0;
+  emails.forEach(async email => {
+    count++
+    try {
+      _sendEmailToSending(email)
+      _checkSendDate(email)
+      _checkTimeout(email)
+      _sendMail(email, count)
+    } catch (err) {
+      console.log(err)
+    }
+  });
+}
+
+// SUB FUNCTIONS CALLED BY MAIN
+
+async function _sendEmailToSending(email){
+  await moveEmailToFolder(graph, email.uuid, "sending");
+}
+
+async function _checkSendDate(email) {
+  if (!email.sentDate) {
+    await createSentDate(graph, email)
+    ;
+    console.log(` > No send date found, a send date has been created.`)
+  }
+}
+
+async function _checkTimeout(email) {
+  let modifiedDate = new Date(email.sentDate);
+  let currentDate = new Date();
+  let timeout = ((currentDate - modifiedDate) / (1000 * 60 * 60)) <= parseInt(hoursDeliveringTimeout);
+
+  if (timeout) {
+    await moveEmailToFolder(graph, email.uuid, "failbox");
+    ;
+    throw `*** FAILED: Timeout reached, message moved to failbox: ${email.uuid} ***`;
+  }
+}
+
+async function _sendMail(email, count) {
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  const attachments = (email.attachments || []).map((attachment) => {
+    return {
+      filename: attachment.filename,
+      path: attachment.dataSource
+    };
+  });
+
+  const mailProperties = {
+    from: `${fromName} ${email.messageFrom}`,
+    to: email.emailTo,
+    cc: email.emailCc,
+    bcc: email.emailBcc,
+    subject: email.messageSubject,
+    text: email.plainTextMessageContent,
+    html: email.htmlMessageContent,
+    attachments: attachments
+  };
+
+  transporter.sendMail(mailProperties, async (failed, success) => {
+    
+  if(failed){
+  // await moveEmailToFolder(graph, email.uuid, "failbox");
+  console.log(` > The destination server responded with an error. Email ${count} send to Failbox.`)
+  console.log(` > ${failed}`)
+
+  } else {
+    console.log(` > Email ${count} UUID:`, email.uuid);
+    await moveEmailToFolder(graph, email.uuid, "sentbox");
+    ;
+    console.log(` > Message moved to sentbox: ${email.uuid}`);
+    await updateEmailId(graph, email.messageId, success.messageId);
+    ;
+    console.log(` > MessageId updated from ${email.messageId} to ${success.messageId}`);
+    email.messageId = success.messageId;
+    console.log(` > Preview URL EMAIL ${count}: %s`, nodemailer.getTestMessageUrl(success));
+  }
+  })
+
 }
 
 export default test;
