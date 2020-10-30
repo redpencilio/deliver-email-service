255fb31ba6031bab54b5e75652df4c33b4fd382f
achraf <achrafatauil@hotmail.com>
2020-10-30 15:43:54 +0100

    add await to test functions

--- services/protocols/TEST.js
+++ services/protocols/TEST.js
@@ -12,30 +12,27 @@ const graph = process.env.GRAPH_NAME || 'http://mu.semte.ch/graphs/system/email'
 
 // MAIN FUNCTION
 async function test(emails){
-  console.log(" >>> Sending mails in Testing Environment");
+  console.log(" >>> PROTOCOL: TEST");
   let count = 0;
-  emails.forEach(email => {
-    count++;
-    try {
-      _sendEmailToSending(email);
-      _checkSendDate(email);
-      _checkTimeout(email);
-      _sendMail(email, count);
-    } catch (err) {
-      console.log(err);
-    }
-  });
+  try {
+    emails.forEach(async email => {
+      count++;
+        await moveEmailToFolder(graph, email.uuid, "outbox");
+        await _checkSendDate(email);
+        await _checkTimeout(email);
+        await _sendMail(email, count);
+    });
+  } catch (err) {
+   console.log(err)
+  }
 }
 
 // SUB FUNCTIONS CALLED BY MAIN
-async function _sendEmailToSending(email){
-  moveEmailToFolder(graph, email.uuid, "sending");
-}
 
 async function _checkSendDate(email) {
   if (!email.sentDate) {
     await createSentDate(graph, email);
-    console.log(` > No send date found, a send date has been created.`);
+    console.log(` >>> No send date found, a send date has been created.`);
   }
 }
 
@@ -43,14 +40,17 @@ async function _checkTimeout(email) {
   let modifiedDate = new Date(email.sentDate);
   let currentDate = new Date();
   let timeout = ((currentDate - modifiedDate) / (1000 * 60 * 60)) <= parseInt(hoursDeliveringTimeout);
-
+  
   if (timeout) {
-    await moveEmailToFolder(graph, email.uuid, "failbox");
+    await moveEmailToFolder(graph, email.uuid, "outbox");
     throw `*** FAILED: Timeout reached, message moved to failbox: ${email.uuid} ***`;
   }
+  
+
 }
 
 async function _sendMail(email, count) {
+  
   let testAccount = await nodemailer.createTestAccount();
 
   // create reusable transporter object using the default SMTP transport
@@ -81,14 +81,17 @@ async function _sendMail(email, count) {
     html: email.htmlMessageContent,
     attachments: attachments
   };
-
+  
   transporter.sendMail(mailProperties, async (failed, success) => {
+    
   if(failed){
+    
   await moveEmailToFolder(graph, email.uuid, "failbox");
   console.log(` > The destination server responded with an error. Email ${count} send to Failbox.`);
   console.dir(` > Email ${count}: ${failed}`);
 
   } else {
+    
     console.log(` > Email ${count} UUID:`, email.uuid);
     moveEmailToFolder(graph, email.uuid, "sentbox");
     console.log(` > Email ${count}: Message moved to sentbox: ${email.uuid}`);
