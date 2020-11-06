da6e73138335bc9b43e2e6eff1d0c61d0dbaee43
achraf <achrafatauil@hotmail.com>
2020-11-06 14:03:07 +0100

    (m2) change let to const since no variable re-assignment

--- services/main.js
+++ services/main.js
@@ -13,7 +13,7 @@ import {
 // MAIN FUNCTION
 async function main(res) {
   try{
-    let emails = await fetchEmails(GRAPH, URI);
+    const emails = await fetchEmails(GRAPH, URI);
 
     if (emails.length == 0) {
       console.log("*** No Emails found to be send. ***")
